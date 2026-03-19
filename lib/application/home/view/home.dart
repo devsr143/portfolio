e76a4b71b0b1ab8f0538d1c8cde97c5044d78291
devsr143 <devachandsr143@gmail.com>
2026-03-19 14:18:15 +0530

    new background

--- lib/application/home/view/home.dart
+++ lib/application/home/view/home.dart
@@ -32,7 +32,7 @@ class _HomeState extends State<Home> {
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/Devachandsr.png"),
+                image: AssetImage("assets/portnew.png"),
                 fit: BoxFit.cover,
               ),
             ),
